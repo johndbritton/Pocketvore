8a50c13aa73e347e6fc7fc5230590b55a29526eb
Conroy Whitney <conroy.whitney@gmail.com>
2010-11-03 15:58:19 -0400

    nearby page working and formatted for both iPhone and Android; is a bit slow... if you start scrolling right after hitting the page, your next set of results is not yet visible... i think this might be related to calling "getNearby" more than once. will look at improving this after getting the Restaurant page done

--- titanium/Resources/windows/nearby.js
+++ titanium/Resources/windows/nearby.js
@@ -1,3 +1,7 @@
+// =====================================================================================
+// UI
+// =====================================================================================
+
 var nearbyHeader = Ti.UI.createImageView({
   image:'../nearby_header.png',
   height:67,
@@ -6,21 +10,47 @@ var nearbyHeader = Ti.UI.createImageView({
   right:0
 });
 
-nearbyHeader.addEventListener('click', function(e){
-  Ti.App.fireEvent('updateGeo');
-});
-
 var nearbyListTableView = Ti.UI.createTableView({
   data:[],
   backgroundColor:'#E5E5DB',
   rowHeight:74,
-  top:67,
-  height:413
+  top:67
 });
+
+// =====================================================================================
+// HELPER FUNCTIONS
+// =====================================================================================
 
 function sortRestaurantsByDistance(a, b){
   return (a.distance_to_center - b.distance_to_center);
 }
+
+function openWindow(url, title, restaurant) {    
+    var win = Ti.UI.createWindow({  
+        url: url,
+        title: title,
+        fullscreen: false,
+        tabBarHidden:true,
+        navBarHidden:true,
+        restaurant: restaurant
+    });
+    
+    if (Ti.UI.currentTab != null) {
+        Ti.UI.currentTab.open(win, {});
+    } else {
+        win.open({});
+    }
+    
+    return win;
+}
+
+// =====================================================================================
+// EVENTS
+// =====================================================================================
+
+nearbyHeader.addEventListener('click', function(e){
+  Ti.App.fireEvent('updateGeo');
+});
 
 // listen for new nearby restaurants
 Ti.App.addEventListener('updateNearby', function(event){
@@ -38,6 +68,17 @@ Ti.App.addEventListener('updateNearby', function(event){
       backgroundImageUrl = '../score.png';      
     } else {
       backgroundImageUrl = '../no_score.png';
+    }
+    
+    // This is PURELY COSMETIC
+    // Sux, I know...
+    // ...but Android will format the rounded_dinescore as a decimal w/ 1 decimal place (XX.0)
+    var formatted_score = '';
+    if (restaurants[i].rounded_dinescore > 0) {
+        formatted_score = '' + restaurants[i].rounded_dinescore;
+        if (formatted_score.indexOf('.') != -1) {
+            formatted_score = formatted_score.substring(0, formatted_score.indexOf('.'));
+        }
     }
 
     var row = Ti.UI.createTableViewRow({
@@ -75,21 +116,21 @@ Ti.App.addEventListener('updateNearby', function(event){
       textAlign:'right',
       top:20,
       right:5,
-      height:14
+      height:16
     });
     
     var cuisine = Ti.UI.createLabel({
       text:restaurants[i].supported_cuisines,
       font:{fontFamily:'Verdana',fontSize:14,fontStyle:'italic'},
       color:'#5E4319',
-      bottom:15,
+      bottom:10,
       left:10,
-      height:18,
+      height:20,
       width:130
     });
     
     var score = Ti.UI.createLabel({
-      text:restaurants[i].rounded_dinescore,
+      text:formatted_score,
       font:{fontFamily:'Georgia',fontSize:24,fontWeight:'bold'},
       color:'#FFFFFF',
       right:5,
@@ -108,19 +149,23 @@ Ti.App.addEventListener('updateNearby', function(event){
     row.className = 'nearby_row';
 
     data.push(row);    
-  }
+  } // end of for loop creating rows
+  
   nearbyListTableView.setData(data);
 });
 
-nearbyListTableView.addEventListener('click', function(e)
-{
-  var win = Ti.UI.createWindow({
-    url:'restaurant.js',
-    title:e.rowData.restaurant.name,
-    restaurant:e.rowData.restaurant
-  });
-  Ti.UI.currentTab.open(win);
+nearbyListTableView.addEventListener('click', function(e) {
+    openWindow('restaurant.js', e.rowData.restaurant.name, e.rowData.restaurant);
 });
+
+// =====================================================================================
+// APP FLOW
+// =====================================================================================
+
+// Whenever this view opens, we should update this list of Nearby restaurants !
+// Forward-firing events (firing updateGeo and then opening this View) do not work for Android (that I could figure out)
+// Instead, we re-fire this event when we load this View so that we are open (and able) to handle it
+Ti.App.fireEvent('updateGeo');
 
 Ti.UI.currentWindow.add(nearbyHeader);
 Ti.UI.currentWindow.add(nearbyListTableView);
